138214d052531f260ec38714dae15286ba25d5f5
xDrakeFP <fpini3q@gmail.com>
2025-10-27 10:37:11 +0100

    Initial commit

---
+++ src/main/java/gruppo1/epicenergy/EpicenergyApplication.java
@@ -0,0 +1,13 @@
+package gruppo1.epicenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicenergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicenergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gruppo1/epicenergy/EpicenergyApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppo1.epicenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicenergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
